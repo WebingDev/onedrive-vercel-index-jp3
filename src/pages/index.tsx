00c6abe06adc1f1809701f0e019e8d1210e0829b
WebingDev <139037943+WebingDev@users.noreply.github.com>
2024-10-06 00:15:40 -0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <title>{siteConfig.title}</title>
       </Head>
 
-      <main className="flex w-full flex-1 flex-col bg-gray-50 dark:bg-gray-800">
+      <main className="flex w-full flex-1 flex-col bg-gray-50 dark:bg-gray-800">r
         <Navbar />
         <div className="mx-auto w-full max-w-5xl py-4 sm:p-4">
           <nav className="mb-4 flex items-center justify-between px-4 sm:px-0 sm:pl-1">
